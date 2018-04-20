e80b53d54f1f57fa08c33f63c09e9c75284c8950
Milder Lisondra <milder.lisondra@yahoo.com>
2018-04-20 12:32:48 -0700

    Many changes

--- api/models/products.php
+++ api/models/products.php
@@ -8,9 +8,11 @@ class Products{
 	private $products_categories = "pd_products_categories";
 	private $applications = "pd_applications";
 	private $attributes = "pd_products_attributes";
+	private $products_families = "pd_products_families";
 	
 	private $view_products = "view_products_by_categories";
 	private $view_products_applications = "view_applications_by_product";
+	private $view_products_by_families = "view_products_by_families";
  
     // object properties
     public $id;
@@ -72,7 +74,12 @@ class Products{
 			$row->pulse_width = $attribs['PULSE_WIDTH'];
 			$row->mode = $attribs['OPERATIONMODE'];
 			$row->url = $attribs['URL'];
-			$row->technology = $attribs['TECHNOLOGY'];			
+			$row->technology = $attribs['TECHNOLOGY'];
+			$row->materials = $attribs['MATERIALS'];
+			$row->material_thickness = $attribs['MATERIAL_THICKNESS'];
+			$row->motion_type = $attribs['MOTION_TYPE'];
+			$row->max_work_area = $attribs['MAX_WORK_AREA'];
+			$row->precision = $attribs['PRECISION'];	
 			$applications = $this->get_applications_by_product( $product_id ); 			
 			$row->application = $applications;
 			$results[$product_id] = $row;
@@ -81,10 +88,18 @@ class Products{
 		return $results;
 	}
 	
-	
+	/**
+	* get_lmc_nav_products
+	* @param string $subcategory
+	*/
 	public function get_lmc_nav_products( $subcategory = "Energy"){
-
-		$stmt = $this->conn->prepare("SELECT * FROM `".$this->view_products."` WHERE `Category_ID` = '43' AND `Show_On_Nav` = 1 AND `Name` LIKE '".$subcategory."%'");
+		
+		if( $subcategory == "Energy" || $subcategory == "Power"){
+			$stmt = $this->conn->prepare("SELECT * FROM `".$this->view_products."` WHERE `Category_ID` = '43' AND `Show_On_Nav` = 1 AND `Name` LIKE '".$subcategory."%'");
+		}else{
+			$stmt = $this->conn->prepare("SELECT * FROM `".$this->view_products."` WHERE `Show_On_Nav` = 1  AND (`Name` LIKE '%labmax%'  OR `Name` LIKE '%FieldMax%' OR `Name` LIKE '%FieldMate%'  OR `Name` LIKE '%LaserCheck%' )");
+		}
+		
 		$stmt->bindValue(':cat', $cat, PDO::PARAM_INT);
 		$stmt->execute();
 		//$stmt->debugDumpParams();
@@ -95,9 +110,18 @@ class Products{
 			$row->detector_diameter = $attribs['DETECTOR_DIAMETER'];
 			$row->min_energy = $attribs['MIN_ENERGY'];
 			$row->max_energy = $attribs['MAX_ENERGY'];
+			$row->wavelength = $attribs['WAVELENGTH'];
 			$row->min_wavelength = $attribs['WAVELENGTH_MIN'];
 			$row->max_wavelength = $attribs['WAVELENGTH_MAX'];
-			$row->max_repetition_rate = $attribs['REPETITION_RATE_MAX'];
+			$row->repetition_rate_max = $attribs['REPETITION_RATE_MAX'];
+			$row->aperture_size = $attribs['ACTIVE_AREA'];
+			$row->pulse_width = $attribs['PULSE_WIDTH_MAX'];
+			$row->power_min = $attribs['POWER_MIN'];
+			$row->power_max = $attribs['POWER_MAX'];
+			$row->cooling_width = $attribs['COOLING_WIDTH'];
+			$row->pc_interface = $attribs['PC_INTERFACE'];
+			$row->measurement_type = $attribs['MEASUREMENT_TYPE'];
+			$row->uncertainty = $attribs['UNCERTAINTY'];
 			$row->url = $attribs['URL'];
 			$row->product_id = $product_id;
 			
@@ -107,7 +131,38 @@ class Products{
 		}
 		return $results;
 	}
-	
+
+	/**
+	* get_lmc_nav_beam_products
+	*
+	*/
+	public function get_lmc_nav_beam_products(){
+		
+		//$stmt = $this->conn->prepare("SELECT * FROM `".$this->products_families."` WHERE `Family_ID` = '13113' AND `Show_On_Nav` = 1 AND `Name` LIKE '".$subcategory."%'");
+		$stmt = $this->conn->prepare("SELECT * FROM `".$this->view_products_by_families."` WHERE `Family_ID` = '13113' AND `Show_On_Nav` = 1");
+		
+		$stmt->bindValue(':cat', $cat, PDO::PARAM_INT);
+		$stmt->execute();
+		//$stmt->debugDumpParams();
+		while ($row = $stmt->fetchObject()) {
+			$product_id = $row->ID;
+			
+			$attribs = $this->get_product_attributes_lmc( $product_id );
+			$row->wavelength = $attribs['WAVELENGTH'];
+			$row->beam_diameter = $attribs['BEAM_DIAMETER'];
+			$row->active_area = $attribs['ACTIVE_AREA'];
+			$row->pixel_size = $attribs['PIXEL_SIZE'];
+			$row->laser_type = $attribs['PULSE_CW'];
+			$row->url = $attribs['URL'];
+			$row->product_id = $product_id;
+			
+			$applications = $this->get_applications_by_product( $product_id ); 
+			$row->application = $applications;
+			$results[$product_id] = $row;			
+		}
+		
+		return $results;
+	}	
 	
 	/**
 	* @param int $id
@@ -116,9 +171,19 @@ class Products{
 	public function get_product_attributes( $id ){		
 		
 		// prepare sql and bind parameters
-		$stmt = $this->conn->prepare("SELECT * FROM `".$this->attributes."` WHERE `Product_ID`  = :id AND (Product_Attribute = 'URL' OR Product_Attribute = 'WAVELENGTH' OR Product_Attribute = 'OPERATIONMODE' OR Product_Attribute = 'TECHNOLOGY' OR Product_Attribute = 'POWER' OR Product_Attribute = 'PULSE_WIDTH')");
+		$stmt = $this->conn->prepare("SELECT * FROM `".$this->attributes."` 
+		WHERE `Product_ID`  = :id 
+		AND 
+		(Product_Attribute = 'URL' 
+		OR Product_Attribute = 'WAVELENGTH' 
+		OR Product_Attribute = 'OPERATIONMODE' 
+		OR Product_Attribute = 'TECHNOLOGY' 
+		OR Product_Attribute = 'POWER' 
+		OR Product_Attribute = 'PULSE_WIDTH' 
+		OR Product_Attribute = 'MATERIALS' 
+		OR Product_Attribute = 'MATERIAL_THICKNESS' OR Product_Attribute = 'MOTION_TYPE' OR Product_Attribute = 'MAX_WORK_AREA' OR Product_Attribute = 'PRECISION')");
 		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-		$stmt->execute();
+		$stmt->execute();	
 		while ($row = $stmt->fetchObject()) {
 			$product_attribute = $row->Product_Attribute;
 			$product_attribute_value = $row->Product_Attribute_Value;
@@ -134,9 +199,27 @@ class Products{
 	* @return array $results
 	*/
 	public function get_product_attributes_lmc( $id ){		
-		
 		// prepare sql and bind parameters
-		$stmt = $this->conn->prepare("SELECT * FROM `".$this->attributes."` WHERE `Product_ID`  = :id AND (Product_Attribute = 'URL' OR Product_Attribute = 'WAVELENGTH_MIN' OR Product_Attribute = 'WAVELENGTH_MAX' OR Product_Attribute = 'DETECTOR_DIAMETER' OR Product_Attribute = 'MIN_ENERGY' OR Product_Attribute = 'MAX_ENERGY' OR Product_Attribute = 'REPETITION_RATE_MAX')");
+		$stmt = $this->conn->prepare(
+		"SELECT * FROM `".$this->attributes."` 
+		WHERE `Product_ID`  = :id 
+		AND (Product_Attribute = 'URL' 
+		OR Product_Attribute = 'WAVELENGTH_MIN' 
+		OR Product_Attribute = 'WAVELENGTH_MAX' 
+		OR Product_Attribute = 'DETECTOR_DIAMETER' 
+		OR Product_Attribute = 'MIN_ENERGY' 
+		OR Product_Attribute = 'MAX_ENERGY' 
+		OR Product_Attribute = 'REPETITION_RATE_MAX' 
+		OR Product_Attribute = 'BEAM_DIAMETER' 
+		OR Product_Attribute = 'ACTIVE_AREA' 
+		OR Product_Attribute = 'PIXEL_SIZE' 
+		OR Product_Attribute = 'PULSE_CW' 
+		OR Product_Attribute = 'WAVELENGTH' 
+		OR Product_Attribute = 'PULSE_WIDTH'
+		OR Product_Attribute = 'POWER_MIN'
+		OR Product_Attribute = 'POWER_MAX' 
+		OR Product_Attribute = 'COOLING_METHOD' 
+		OR Product_Attribute = 'PC_INTERFACE' OR Product_Attribute = 'MEASUREMENT_TYPE' OR Product_Attribute = 'UNCERTAINTY')");
 		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 		$stmt->execute();
 		//$stmt->debugDumpParams();
